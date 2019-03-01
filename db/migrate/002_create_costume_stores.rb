8590ec6a813d93aed2da493dc4166672795048ae
Laura Felce <crepu83@yahoo.it>
2019-03-01 11:55:10 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees_count
+      t.boolean :still_in_business
+      t.integer :opening_time
+      t.integer :closing_time
+    end
+  end
+end
